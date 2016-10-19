d1c1914ddd467b788a912303655628d61cc73280
fcschmidt <fcschimidt@gmail.com>
2016-10-19 16:43:44 -0200

    Site optimized

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     /* Open mobile menu */
     $('#btn__menu').eq(0).click(function () {
-        $('#mobile__menu').eq(0).slideToggle(500);
+        $('.nav__menu').eq(0).slideToggle(500);
     });
 });
