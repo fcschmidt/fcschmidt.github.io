378550fc239dfd53e327a9f68319fdb63f20ee6e
fcschmidt <fcschimidt@gmail.com>
2017-04-27 13:19:46 -0300

    Update stylus and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,9 @@ $(document).ready(function () {
         }, 'slow');
     });
 
-    // Clipboard js
-    new Clipboard('.btn');
-
     // Scroll navbar and indice for Learning page
     $(window).scroll(function(){
         if($(document).scrollTop() > 160){
-
             $('#navbar').css({
                 'width' : '100%',
                 'position' : 'fixed',
@@ -45,6 +41,80 @@ $(document).ready(function () {
                 'width' : 'auto',
                 'position' : 'relative',
                 'top': '0'
+            });
+        }
+    });
+    // Nav Trilha - Single post
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 50){
+            $('.nav__trilha').css({
+                'width' : '100%',
+                'position' : 'fixed',
+                'top' : '0',
+                'right' : '0',
+                'left' : '0',
+                'padding-top' : '1.5em',
+                'z-index' : '1030'
+            });
+
+            $('#toTop').css({
+                'display' : 'block'
+            });
+        } else {
+            $('.nav__trilha').css({
+                'position' : 'relative',
+                'top' : 'auto',
+                'padding-top' : '0'
+            });
+
+            $('#toTop').css({
+                'display' : 'none'
+            });
+        }
+    });
+    /*
+    * Navbar Secondary Menu and Avatar display
+    * Error Media Queries
+    * Adicionar regras de media queries para visualização
+    */
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 100){
+            $('#second').css({
+                'width' : '100%',
+                'position' : 'fixed',
+                'top' : '0',
+                //'right' : '0',
+                //'left' : '0',
+                'padding-top' :'1em',
+                'z-index' : '1030'
+            });
+        } else {
+           $('#second').css({
+                'width' : '50%',
+                'position' : 'relative',
+                'padding-top' : '.3em'
+           });
+        }
+    });
+    $(window).scroll(function(){
+        if($(document).scrollTop() > 300){
+            $('#second').css({
+                'width' : '100%',
+                'position' : 'fixed',
+                'top' : '0',
+                //'right' : '0',
+                'left' : '0',
+                'padding-top' : '1em',
+                'z-index' : '1030'
+            });
+
+            $('#avatar').css({
+                'display' : 'block',
+                'margin-right' : '3.4em'
+            });
+        } else {
+            $('#avatar').css({
+                'display' : 'none'
             });
         }
     });
